3693cb74d74661e35ce952f771881f4794b2e673
wise-create <63413271+wise-create@users.noreply.github.com>
2020-08-18 09:16:32 +0900

    1.3
    슬라이드 작동

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,83 +1,91 @@
 (function($){
 
-    function openNav() {
+    function openNav(){
         $('#header').toggleClass('on')
         if ( $('#header').hasClass('on') ) {
             $('.nav').css({
-                display:'block'
+                display: 'block'
             }).animate({
-                right:'0px'
+                right: 0
             }, 500)
         } else {
             $('.nav').animate({
-                right:'-320px'
+                right: '-320px'
             }, 500, function(){
                 $(this).css({
-                    display:'none'
+                    display: 'none'
                 })
             })
         }
-        $('.outlater').toggleClass('on')
+        $('.outlayer').toggleClass('on')
     }
     $('.open-gnb').on('click', openNav)
     $('.outlayer').on('click', openNav)
 
+    /* $(window).resize(function(){
+        var winWidth = $(this).innerWidth()
+        if ( winWidth > 700 ) {
+            $('#header').removeClass('on')
+            $('.nav').css({
+                display: 'block',
+                right: 0
+            })
+        } else if ( winWidth <= 700 ) {
+            $('#header').removeClass('on')
+            $('.nav').css({
+                display: 'none',
+                right: '-320px'
+            })
+        }
+    }) */
 
-    function init() {
-        var winWidth = $(window).innerWidth()
-        if ( winWidth>700 && !$('html').hasClass('pc') ) {
+    function init(){
+        var winWidth = $(this).innerWidth()
+        if ( winWidth > 700 && !$('html').hasClass('pc') ) {
             $('#header').removeClass('on')
             $('.outlayer').removeClass('on')
             $('.nav').css({
-                display:'block',
-                right:'0px'
+                display: 'block',
+                right: '0px'
             })
             $('html').addClass('pc').removeClass('mobile')
-        } else if (winWidth<700 && !$('html').hasClass('mobile')) {
+        } else if ( winWidth <= 700 && !$('html').hasClass('mobile') ) {
             $('#header').removeClass('on')
             $('.nav').css({
-                display:'none',
-                right:'-320px'
+                display: 'none',
+                right: '-320px'
             })
             $('html').addClass('mobile').removeClass('pc')
         }
     }
-
     init()
 
     $(window).resize(function(){
         init()
     })
 
+    // 메인슬라이드 : 슬릭슬라이드 연결
     $('.slide-inner').slick({
         autoplay: true,
-        dots: true,
-        autoplaySpeed: 4500,
+        autoplaySpeed: 3000,
         speed: 600,
-        slidesToShow:1,
-        slidesToScroll:1,
-        pauseOnHover:true,
-        pauseOnDotsHover:false,
-        pauseOnFocus:false,
-        cssEase:'ease',
-        draggable:true,
-        fade:false, 
-        arrows:true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        pauseOnDotsHover: true,
+        pauseOnFocus: false,
+        cssEase: 'linear',
+        draggable: true,
+        fade: false,
+        arrows: true,
         prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
-        nextArrow:'<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>'
+        nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>'
     })
-    
-    $(".plpa").toggle(
-        function(){
-            $(this).find('i').removeClass('fa-pause')
-            .addClass('fa-play')
-            $(".slide-inner").slick("slickPause")
-        },
-        function(){
-            $(this).find('i').removeClass('fa-play')
-            .addClass('fa-pause')
-            $(".slide-inner").slick("slickPlay")
-        } 
-    )
 
-})
+
+
+
+
+
+})(jQuery)
